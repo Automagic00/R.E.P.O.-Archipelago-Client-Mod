41f15246535a26510726b8cb19297c4345595c28
Devin <45304757+Automagic00@users.noreply.github.com>
2025-06-24 18:21:49 -0500

    Delete obj directory

--- obj/Debug/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RepoAP
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "RepoAP";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
