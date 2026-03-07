92f974a1a9207a4ff9b17e3f6da6cbf0cb5178c1
IThisGuyHere <cnijdl1@gmail.com>
2026-03-07 15:14:03 -0700

    upgrade spawning improvements
    the patch now duplicates only the item volumes, so the original method can handle spawning
    moved the new spawn spots to the shelf on the side of the truck
    added space for 96 additional upgrades, bringing the total to 112

--- Core/UpgradeSpawningPatch.cs
+++ Core/UpgradeSpawningPatch.cs
@@ -13,87 +13,38 @@ namespace RepoAP.Core
 {
     internal class UpgradeSpawningPatch
     {
+        const int MAX_UPGRADE_ROWS = 3;
+        const int MAX_UPGRADES_PER_ROW = 32;
+
+        /**
+         * The goal here is to create new spots for upgrades to spawn along the shelf on the side of the truck.
+         */
         [HarmonyPatch(typeof(PunManager), nameof(PunManager.TruckPopulateItemVolumes))]
         [HarmonyPrefix]
-        internal static void PopulateItemVolumesPrefix(PunManager __instance, ItemManager ___itemManager, out List<Vector3> __state)
+        internal static void PrepareNewTruckItemVolumes(PunManager __instance, ItemManager ___itemManager)
         {
-            //ItemManager itemManager = (ItemManager)AccessTools.Field(typeof(PunManager), "itemManager").GetValue(__instance);
-            //__state = new List<Vector3>(itemManager.itemVolumes.Select(volume => volume.transform.position));
-            //Plugin.Logger.LogInfo($"Original Item Volumes Count: {__state.Count}");
-            //Plugin.Logger.LogInfo($"Original objList Count: {itemManager.purchasedItems.Count}");
-            
-            List<Vector3> validSpawnSpots = new();
-            /*List<Item> objList = [.. itemManager.purchasedItems];
-            for (int i = 0; i < objList.Count; ++i)
+            List<ItemVolume> upgradeItemVolumeList = [.. ___itemManager.itemVolumes.Where(volume => volume.itemVolume == SemiFunc.itemVolume.upgrade)];
+            if (upgradeItemVolumeList.Count < 16) return;    // just a precaution
+            Vector3 directionToSpawnUpgrades = Vector3.Cross(upgradeItemVolumeList[7].transform.position, upgradeItemVolumeList[8].transform.position).normalized;   // this is originally (0.00, 1.00, 0.09) at game start. after, it is (0.00, 1.00, -0.09) in levels and (0.09, 1.00, 0.00) on the road
+            directionToSpawnUpgrades.y = 0;
+            if (directionToSpawnUpgrades.z > 0) directionToSpawnUpgrades.z *= -1;
+            if (directionToSpawnUpgrades.x > 0) directionToSpawnUpgrades.x *= -1;
+            Vector3 distBetweenUpgrades = upgradeItemVolumeList[7].transform.position - upgradeItemVolumeList[8].transform.position;    // these two are right next to one another, which is needed
+            // when you load a save, the upgrade volumes are at z=-14.02. all other times, they are either at -19.28 or -16.78
+            Vector3 newRowStartPosition = (upgradeItemVolumeList[7].transform.position + directionToSpawnUpgrades * 26f) - distBetweenUpgrades;
+
+            List<ItemVolume> newUpgradeItemVolumes = [];
+            for (int i = 0; i < MAX_UPGRADE_ROWS; i++)
             {
-                Item item = objList[i];
-                ItemVolume matchingVol = itemManager.itemVolumes.Find(v => v.itemVolume == item.itemVolume);
-                if (matchingVol != null)
+                Vector3 newRowPosition = newRowStartPosition;
+                for (int j = 0; j < MAX_UPGRADES_PER_ROW; j++)
                 {
-                    validSpawnSpots.Add(matchingVol);
-                    objList.RemoveAt(i);
+                    ItemVolume newVolume = GameObject.Instantiate(upgradeItemVolumeList[7], newRowPosition += directionToSpawnUpgrades, upgradeItemVolumeList[8].transform.rotation);
+                    newUpgradeItemVolumes.Add(newVolume);
                 }
-            }*/
-            List<ItemVolume> itemVolumeList = [..___itemManager.itemVolumes];
-            List<Item> objList = [.. ___itemManager.purchasedItems];
-
-            while (itemVolumeList.Count > 0 && objList.Count > 0)
-            {
-                bool flag = false;
-                for (int index = 0; index < objList.Count; ++index)
-                {
-                    Item item = objList[index];
-                    ItemVolume volume = itemVolumeList.Find(v => v.itemVolume == item.itemVolume && v.itemVolume == SemiFunc.itemVolume.upgrade);
-                    if (volume)
-                    {
-                        validSpawnSpots.Add(volume.transform.position);
-                        itemVolumeList.Remove(volume);
-                        objList.RemoveAt(index);
-                        flag = true;
-                        break;
-                    }
-                }
-                if (!flag)
-                    break;
+                newRowStartPosition += distBetweenUpgrades;
             }
-            Plugin.Logger.LogInfo($"validSpawnSpots Count: {validSpawnSpots.Count}");
-            __state = validSpawnSpots;
-        }
-        [HarmonyPatch(typeof(PunManager), nameof(PunManager.TruckPopulateItemVolumes))] // this doesn't work. We need to do a transpiler patch because purchasedUpgrades is copied into another list inside the method
-        [HarmonyPostfix]
-        internal static void PopulateItemVolumesPostfix(PunManager __instance, ref ItemManager ___itemManager, List<Vector3> __state)
-        {
-            Plugin.Logger.LogInfo($"purchasedItems Count before: {___itemManager.purchasedItems.Count}");
-            if (__state.Count < 2) return;
-            Quaternion rotation = ShopManager.instance.itemRotateHelper.transform.rotation;
-            Vector3 directionToSpawnUpgrades = ShopManager.instance.itemRotateHelper.transform.rotation * Vector3.Cross(__state[0], __state[1]).normalized;
-            Plugin.Logger.LogInfo($"Direction to spawn upgrades: {directionToSpawnUpgrades}. Position of first shop slot: {__state[0]}. Position of new shop slot: {__state[0] + directionToSpawnUpgrades}");
-            //ItemManager ___itemManager = (ItemManager)AccessTools.Field(typeof(PunManager), "itemManager").GetValue(__instance);
-            List<Item> purchasedUpgrades = [..___itemManager.purchasedItems.Where(i => i.itemVolume == SemiFunc.itemVolume.upgrade)];
-            int upgradesToPlace = Math.Min(purchasedUpgrades.Count, __state.Count);
-
-            for (int i = 0; i < upgradesToPlace; i++)
-            {
-                SpawnItemAtPosition(purchasedUpgrades[i], __state[i] + directionToSpawnUpgrades);
-                ___itemManager.purchasedItems.Remove(purchasedUpgrades[i]);
-            }
-            Plugin.Logger.LogInfo($"purchasedItems Count after: {___itemManager.purchasedItems.Count}");
-        }
-
-        public static void SpawnItemAtPosition(Item item, Vector3 position)    // borrowed from NoItemSpawnLimit
-        {
-            ShopManager.instance.itemRotateHelper.transform.parent = ShopManager.instance.transform;
-            ShopManager.instance.itemRotateHelper.transform.localRotation = item.spawnRotationOffset;
-            Quaternion rotation = ShopManager.instance.itemRotateHelper.transform.rotation;
-
-            if (SemiFunc.IsMasterClient())
-            {
-                PhotonNetwork.InstantiateRoomObject(item.prefab.ResourcePath, position, rotation, 0);
-            }
-            else if (!SemiFunc.IsMultiplayer())
-            {
-                UnityEngine.Object.Instantiate(item.prefab.Prefab, position, rotation);
-            }
+            ___itemManager.itemVolumes.AddRange(newUpgradeItemVolumes);
         }
 
     }
