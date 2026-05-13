fdb82ed99afc42746a62feac80ba583d6b6d7d40
IThisGuyHere <cnijdl1@gmail.com>
2026-05-13 12:11:19 -0700

    fixed rerolled ap items not displaying their name for clients
    Archipelago Items now have an ItemRPC component attached to the prefab instead of attaching a CustomRPC component in ItemAttributes.Start
    When a client receives the name change rpc, it waits one update cycle before applying the new name. This prevents te name from being overwritten in ItemAttributes.Start
    AP Items now replace upgrades in ShopManager.GetAllItemsFromStatsManager instead of overriding the spawn attempt

---
+++ Core/ItemRPCs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Photon.Pun;
+using UnityEngine;
+
+namespace RepoAP.Core
+{
+    internal class ItemRPCs : MonoBehaviour
+    {
+
+        public void CallUpdateItemNameRPC(string name, GameObject inst)
+        {
+            Plugin.Logger.LogInfo("Calling UpdateItemNameRPC");
+            PhotonView photonView = inst.GetComponent<PhotonView>();
+            object[] p = new object[] { name };
+            photonView.RPC(nameof(UpdateItemNameRPC), RpcTarget.AllBuffered, p);
+        }
+
+        [PunRPC]
+        public void UpdateItemNameRPC(string name, PhotonMessageInfo info)
+        {
+            Plugin.Logger.LogInfo("UpdateItemNameRPC Called");
+            ItemAttributes inst = info.photonView.gameObject.GetComponent<ItemAttributes>();
+            if (inst == null)
+            {
+                Plugin.Logger.LogError("UpdateItemNameRPC: Item lacks an ItemAttributes component and its name cannot be modified.");
+                return;
+            }
+            IEnumerator coroutine = SetItemNameDelayed(inst, name);
+            StartCoroutine(coroutine);
+        }
+
+        private IEnumerator SetItemNameDelayed(ItemAttributes inst, string newItemName)
+        {
+            FieldInfo field = AccessTools.Field(typeof(ItemAttributes), "itemName");
+            yield return null;
+            field.SetValue(inst, newItemName.Replace("_", " "));
+        }
+    }
+}
